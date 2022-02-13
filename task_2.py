9c6480188287e35d84f9226b7f5bdeb0587800fd
Alibaba <alexandr224@yandex.ru>
2022-02-13 15:46:15 +0300

    task_2

--- task_2.py
+++ task_2.py
@@ -1,1 +1,14 @@
-print()
+mas = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
+ind_list = []
+for ind,el in enumerate(mas):
+    if el.isdigit():
+        mas[ind] = f'{int(el):02d}'
+        ind_list.append(ind)
+    elif el[1:].isdigit():
+        mas[ind] = el[0] + f'{int(el):02d}'
+        ind_list.append(ind)        
+for ind,el in enumerate(mas):
+    if ind in ind_list:
+        print("\"" + el + "\"", end=" ")
+    else:
+        print(el, end=" ")
